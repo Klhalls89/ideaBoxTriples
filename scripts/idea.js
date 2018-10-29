d9494b5550c2af280b154f45eccdd9fe2aa62f0b
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2018-10-29 17:58:59 -0600

    Update voting functionality and started search

--- scripts/idea.js
+++ scripts/idea.js
@@ -2,13 +2,14 @@ class Idea {
   constructor(title, body, id, quality) {
     this.title = title;
     this.body = body;
-    this.quality = 'Swill';
+    this.quality = quality || 'Swill';
     this.id = id || Date.now();
     this.qualityIndex = 0;
   }
 
   
   saveToStorage(array) {
+    localStorage.clear();
     var stringArray = JSON.stringify(array);
     localStorage.setItem('ideasKey', stringArray);
   }
@@ -25,17 +26,34 @@ class Idea {
 
   }
 
-  updateSelf() {
+  updateSelf(title, body) {
+    this.title = title;
+    this.body = body;
   // update title or body on idea card
   }
 
-  updateQuality() {
- var qualityArray = ['Swill','Plausible','Genius'];
-  if (this.qualityIndex <= 1) {
-    this.qualityIndex++
+  updateQuality(vote) {
+    var qualityArray = ['Swill','Plausible','Genius'];
+
+    if (vote === 'up') {
+      if (this.qualityIndex <= 1) {
+        this.qualityIndex++
+      } else if(this.qualityIndex === 2) {
+        return
+        }
+    }
+   
+    if (vote === 'down') {
+      if (this.qualityIndex >= 1) {
+        this.qualityIndex--
+      } else if(this.qualityIndex === 0) {
+        return
+        }
+    }
+
+    this.quality = qualityArray[this.qualityIndex];
   }
-  this.quality = qualityArray[this.qualityIndex];
-  }
+
 }
 
 // module.exports = Idea;
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ var saveBtn = document.querySelector('.js-save-btn');
 var cardRepo = document.querySelector('.js-card-repo');
 var titleInput = document.querySelector('.js-title-input');
 var bodyInput = document.querySelector('.js-body-input');
+var searchInput = document.querySelector('.js-search-bar');
 var ideaArray = [];
 
 setInitState();
@@ -16,9 +17,36 @@ function setInitState() {
 
 saveBtn.addEventListener('click', createNewIdea);
 cardRepo.addEventListener('click', functionCaller);
+cardRepo.addEventListener('keyup', function(event){ 
+  if (event.keyCode === 13) {
+    cardUpdate();
+  }
+ });
+searchInput.addEventListener('keyup', filterCards);
+
+function filterCards() {
+
+  var allCards = document.querySelectorAll('.js-card');
+  console.log(allCards);
+
+  allCards.forEach(function(card) {
+    var title = card.children[0].children[0];
+    var body = card.children[0].children[1];
+    
+    if ( !title.innerText.includes(searchInput.value) || !body.innerText.includes(searchInput.value)) {
+      console.log(card)
+      card.classList.add('hidden');
+    } else {
+      card.classList.remove('hidden');
+    }
+
+  })
+
+}
 
 function functionCaller(){
   upvote();
+  downvote();
   deleteCard();
 }
 
@@ -26,21 +54,38 @@ function upvote(){
   if (event.target.classList.contains('js-upvote')) {
   var cardKey = event.target.closest('.js-card').dataset.key;
   cardKey = parseInt(cardKey);
-  var foundIdea;
 
   ideaArray.forEach(function(ideaInst){
     if (ideaInst.id === cardKey) {
-      ideaInst.updateQuality();
-      foundIdea = ideaInst; 
+      var vote = 'up';
+      ideaInst.updateQuality(vote); 
+      cardKey = cardKey.toString();
+      document.getElementById(cardKey).innerText = `Quality: ${ideaInst.quality}`;
+      ideaInst.saveToStorage(ideaArray);
       }
     });
   }
+}
+
+function downvote(){
+  if (event.target.classList.contains('js-downvote')) {
+  var cardKey = event.target.closest('.js-card').dataset.key;
+  cardKey = parseInt(cardKey);
+
+  ideaArray.forEach(function(ideaInst){
+    if (ideaInst.id === cardKey) {
+      var vote = 'down';
+      ideaInst.updateQuality(vote); 
       cardKey = cardKey.toString();
-      document.getElementById(cardKey).innerText = `Quality: ${foundIdea.quality}`;
+      document.getElementById(cardKey).innerText = `Quality: ${ideaInst.quality}`;
+      ideaInst.saveToStorage(ideaArray);
+      }
+    });
+  }
 }
 
 
-function deleteCard(event){
+function deleteCard(){
  if (event.target.classList.contains('js-delete')) {
   var cardKey = event.target.closest('.js-card').dataset.key;
   cardKey = parseInt(cardKey);
@@ -64,12 +109,33 @@ function createNewIdea() {
    clearInputs(); 
 }
 
+function cardUpdate() {
+  alert('hey there');
+  // if (event.target.classList.contains('js-card-inputs')) {
+  //   var cardKey = event.target.closest('.js-card').dataset.key;
+  //   cardKey = parseInt(cardKey);
+
+  //   var cardTitleInput = document.querySelector('.js-card-title-input');
+  //   var cardBodyInput = document.querySelector('.js-card-body-input');
+
+  //   console.log(cardTitleInput);
+  //   console.log(cardBodyInput);
+
+  //   // ideaArray.forEach(function(ideaInst){
+  //   //   if (ideaInst.id === cardKey) {
+  //   //     ideaInst.updateSelf(cardTitleInput, cardBodyInput);
+  //   //   }
+  //   // });
+
+  // }
+}
+
 function cardPrepend(id, title, body, quality) {
   cardRepo.insertAdjacentHTML('afterbegin',
     `<section data-key="${id}" class="idea-card-sect js-card">
           <article class="card-art">
-            <p class="title-card-style">${title}</p>
-            <p>${body}</p>
+            <p class="title-card-style js-card-inputs">${title}</p>
+            <p class="js-card-inputs">${body}</p>
           </article>
           <article class="quality-art">
             <img class="card-btns js-downvote" src="./assets/downvote.svg">
@@ -96,7 +162,7 @@ function reinstanciateParseCardArray() {
 
   var jsonIdeaArray = JSON.parse(ideaArrayString); 
   jsonIdeaArray.forEach(function(ideaInst) {
-    cardPrepend(ideaInst.id, ideaInst.title, ideaInst.body);
+    cardPrepend(ideaInst.id, ideaInst.title, ideaInst.body, ideaInst.quality);
     var idea = new Idea(ideaInst.title, ideaInst.body, ideaInst.id, ideaInst.quality);
 
     ideaArray.push(idea);
